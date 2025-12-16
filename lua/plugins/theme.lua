1a4cf0963be86b88203667d14fdfba3cc534070d
Adrián Romero <aromero@trevenque.es>
2025-12-16 18:56:38 +0100

    refactor(theme): added tokyonight theme

--- lua/plugins/theme.lua
+++ lua/plugins/theme.lua
@@ -1,10 +1,9 @@
 return {
-  {
-    "catppuccin/nvim",
-    name = "catppuccin",
-    priority = 1000,
-    config = function()
-        vim.cmd.colorscheme("catppuccin")
-    end
-  }
+    {
+        "folke/tokyonight.nvim",
+        priority = 1000,
+        config = function()
+            vim.cmd.colorscheme("tokyonight-night")
+        end,
+    },
 }
